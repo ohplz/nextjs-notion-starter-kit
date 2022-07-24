3413783e8ab39f6060fedca9895e3ff1dd679dcb
ohplz <46417244+ohplz@users.noreply.github.com>
2022-07-24 11:34:10 +0800

    enable header dark mode

--- site.config.ts
+++ site.config.ts
@@ -49,9 +49,9 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
+//   navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
   //   {
   //     title: 'About',
   //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
@@ -60,5 +60,5 @@ export default siteConfig({
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
   //   }
-  // ]
+  ]
 })
