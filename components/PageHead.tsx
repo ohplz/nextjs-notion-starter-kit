9f4e535ed1aa29fd664c748d5226556490c5a852
ohplz <46417244+ohplz@users.noreply.github.com>
2022-07-24 21:25:56 +0800

    新增web-app启动动画图标适配

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -83,6 +83,7 @@ export const PageHead: React.FC<
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <link rel ="apple-touch-icon" href="https://raw.githubusercontent.com/ohplz/nextjs-notion-starter-kit/main/public/apple-web-app-icon.png"/>
+      <link rel ="apple-touch-startup-image" href ="https://raw.githubusercontent.com/ohplz/nextjs-notion-starter-kit/main/public/apple-web-app-icon.png">
       
       <title>{title}</title>
     </Head>
